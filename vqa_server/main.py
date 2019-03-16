51541cd0b3a87095edb89005bfec1d4b78b4cc38
Teodor Stanishev <teodorstanishev@Teodors-MacBook-Pro.local>
2019-03-16 23:38:35 +0200

    Final working VQA server and 6it

--- vqa_server/main.py
+++ vqa_server/main.py
@@ -1,5 +1,6 @@
-from flask import Flask , request
+from flask import Flask , request, make_response
 import base64
+import os
 from PIL import Image
 from vqa_pretrained import vqa
 
@@ -12,9 +13,13 @@ def sendImage():
     img = base64.b64decode(req["imgData"])   
     image = Image.frombytes("RGB", (320, 240), img)
     image.save("image.jpg")
-    print req["question"]
-    vqa.main("image.jpg" , str(req["question"]))
-    return "BACE"
-    
+    print "Question is " + req["question"]
+    os.system("cd vqa_pretrained; python vqa.py -image_file_name ../image.jpg -question \"" + req["question"] +"\"")
+    res = ''
+    with open("vqa_pretrained/output.txt" , 'r') as f:
+        res = f.read()
+    print res
+    return make_response(res , 200)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
