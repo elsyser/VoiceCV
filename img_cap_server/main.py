b92b8d07c5bd773d0bfa77757ce14da6df6af122
Victor Velev <velev.victor@yahoo.com>
2019-03-17 04:06:08 +0200

    [Update] test/serve

--- img_cap_server/main.py
+++ img_cap_server/main.py
@@ -6,11 +6,10 @@ from PIL import Image
 from flask import Flask, make_response, request
 from img_cap.main import greedy_search_inference
 
-# from vqa_pretrained import vqa
 app = Flask(__name__)
 
 
-@app.route("/sendImage" , methods=["POST"])
+@app.route('/sendImage' , methods=['POST'])
 def sendImage():
     req = request.get_json(force=True)
     img = base64.b64decode(req['imgData'])   
