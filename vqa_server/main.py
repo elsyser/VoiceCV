d0e253d020924294a01df42ddb74bd1631230d05
Teodor Stanishev <teodorstanishev@Teodors-MacBook-Pro.local>
2019-03-17 08:19:15 +0200

    Added docs

--- vqa_server/main.py
+++ vqa_server/main.py
@@ -7,15 +7,26 @@ from vqa_pretrained import vqa
 # from vqa_pretrained import vqa
 app = Flask(__name__)
 
+# http://url:5000/sendImage
+# data = {
+#   tag: {string},
+#   imgData: {base64 string},
+#   question: {string}
+# }
 @app.route("/sendImage" , methods=["POST"])
 def sendImage():
+    #Parse the request to json
     req = request.get_json(force=True)
+    #Decode base64 to image file
     img = base64.b64decode(req["imgData"])   
     image = Image.frombytes("RGB", (320, 240), img)
+    #Save the image to file system
     image.save("image.jpg")
-    print "Question is " + req["question"]
-    os.system("cd vqa_pretrained; python vqa.py -image_file_name ../image.jpg -question \"" + req["question"] +"\"")
+    question = req["question"]
+    print "Question is " + question
+    os.system("cd vqa_pretrained; python vqa.py -image_file_name ../image.jpg -question \"" + question +"\"")
     res = ''
+    # Read the outputed file of vqa.py and send it as response
     with open("vqa_pretrained/output.txt" , 'r') as f:
         res = f.read()
     print res
